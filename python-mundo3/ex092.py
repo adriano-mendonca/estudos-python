a123393b29adf4351c69e118cd951578f1e3c6e8
Adriano Brito de Mendonça <62914181+abm-astro@users.noreply.github.com>
2021-08-18 10:19:24 -0300

    Update ex092.py

--- python-mundo3/ex092.py
+++ python-mundo3/ex092.py
@@ -4,15 +4,15 @@ cadastro = dict()
 cadastro['nome'] = str(input('Nome: '))
 cadastro['nascimento'] = int(input('Ano de Nascimento: '))
 cadastro['ctps'] = int(input('Carteira de Trabalho [0 não tem]: '))
-
 if cadastro['ctps'] != 0:
-  print(10*'-=')
   cadastro['contratacao'] = int(input('Ano de Contratação: '))
   cadastro['salario'] = float(input('Salário: R$ '))
   cadastro['aposentadoria'] = 35 - (anoAtual - cadastro['contratacao']) + (anoAtual - cadastro['nascimento'])
+  print(10*'-=')
   for k, v in cadastro.items():
     print(f'{k} tem o valor {v}')
 else:
+  print(10*'-=')
   for k, v in cadastro.items():
     print(f'{k} tem o valor {v}')
   
