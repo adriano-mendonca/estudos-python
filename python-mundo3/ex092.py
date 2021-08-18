a155261bc305c93b7ff7289c5819983d3edb2356
Adriano Brito de Mendonça <62914181+abm-astro@users.noreply.github.com>
2021-08-18 10:17:38 -0300

    construção ex92

--- python-mundo3/ex092.py
+++ python-mundo3/ex092.py
@@ -1,1 +1,18 @@
+from datetime import date
+anoAtual = date.year
+cadastro = dict()
+cadastro['nome'] = str(input('Nome: '))
+cadastro['nascimento'] = int(input('Ano de Nascimento: '))
+cadastro['ctps'] = int(input('Carteira de Trabalho [0 não tem]: '))
 
+if cadastro['ctps'] != 0:
+  print(10*'-=')
+  cadastro['contratacao'] = int(input('Ano de Contratação: '))
+  cadastro['salario'] = float(input('Salário: R$ '))
+  cadastro['aposentadoria'] = 35 - (anoAtual - cadastro['contratacao']) + (anoAtual - cadastro['nascimento'])
+  for k, v in cadastro.items():
+    print(f'{k} tem o valor {v}')
+else:
+  for k, v in cadastro.items():
+    print(f'{k} tem o valor {v}')
+  
