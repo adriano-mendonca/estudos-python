66fa0a6fbbf7fcf710acf879a3f493e37727a0f3
Adriano Brito de Mendonça <62914181+abm-astro@users.noreply.github.com>
2021-08-18 22:12:57 -0300

    Update ex094.py

--- python-mundo3/ex094.py
+++ python-mundo3/ex094.py
@@ -1,35 +1,37 @@
 cadastro = dict()
-cadastroLista = list()
-contador = 0
-idades = 0
-mulheres = list()
-acimaMedia = list()
+pessoas = list()
+soma = 0
 while True:
+  cadastro.clear()
   cadastro['nome'] = str(input('Nome: ')).capitalize()
-  cadastro['sexo'] = str(input('Sexo [M/F]: ')).upper()
+  while True:
+    cadastro['sexo'] = str(input('Sexo [M/F]: ')).upper()
+    if cadastro['sexo'] in 'MF':
+      break
+    print('ERRO! Digite apenas M ou F!')
   cadastro['idade'] = int(input('Idade: '))
-  contador += 1
-  idades += cadastro['idade']
-  cadastroLista.append(cadastro.copy())
-  if cadastro['sexo'] in "Ff":
-    mulheres.append(cadastro['nome'])
-  resposta = str(input('Quer continuar? [S/N] '))
-  if resposta in 'Nn':
+  soma += cadastro['idade']
+  pessoas.append(cadastro.copy())
+  while True:
+    res = str(input('Quer continuar? [S/N] ')).upper()
+    if res in 'SN':
+      break
+    print('ERRO! Digite apenas S ou N!')
+  if res in "Nn":
     break
-mediaIdades = idades/contador
-print(15*'-=')
-print(f'- O grupo tem {contador} pessoas.')
-print(f'- A média de idade é de {mediaIdades:.2f} anos.')
-print('- As mulhes cadastradas foram:', end=' ')
-for c in mulheres:
-	print(c, end='; ')
+print(20*'-=')
+media = soma / len(pessoas)
+print(f'- Ao todo temos {len(pessoas)} pessoas cadastradas.')
+print(f'- A média de idade é de {media:5.2f} anos.')
+print('- As mulheres cadastradas foram:', end=' ')
+for m in pessoas:
+  if m['sexo'] in 'F':
+    print(f"{m['nome']}", end=" ; ")
 print()
-print('- Lista das pessoas que estão acima da média:')
-for p in range(0, len(cadastroLista)):
-	if cadastroLista[p]['idade'] > mediaIdades:
-		acimaMedia.append(cadastroLista[p])
-for x in range(0, len(acimaMedia)):
-	for k, v in acimaMedia[x].items():
-		print(f'{k} = {v}', end= ' ; ')
-print()
+print('- A lista de pessoas acima da média:')
+for p in pessoas:
+  print('  ')
+  for k, v in p.items():
+    if p['idade'] > media:
+      print(f'{k} = {v}', end=' ')
 print('\n>> ENCERRADO <<')
